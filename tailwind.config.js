b1a66adf12ba55c34e1707d35c376b96d0bf4d6c
MaxTGM <jaimaini3@gmail.com>
2022-02-13 12:40:12 +0530

    fonts and responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
+    fontFamily:{
+      'sans': 'DM Sans',
+      'poppins': 'Poppins, sans-serif',
+    },
     extend: {},
   },
   plugins: [],
