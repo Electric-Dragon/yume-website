6b920fe528bf09153fb031def1bf42016a15b7b0
MaxTGM <jaimaini3@gmail.com>
2022-02-16 10:29:53 +0530

    signup

--- app.js
+++ app.js
@@ -15,6 +15,12 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/html/form.html");
 });
 
+app.get("/ss", function(req, res) {
+    res.sendFile(__dirname + "/html/signup.html");
+});
+
+
+
 
 app.listen(process.env.PORT || port, function() {
     console.log(`Server started on http://localhost:${port}`);
