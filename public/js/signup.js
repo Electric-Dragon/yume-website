90d5567f7a3e66e5c8c201fef266f3f7fedb1caa
Atharva Wasekar <45729391+Electric-Dragon@users.noreply.github.com>
2022-02-16 17:12:24 +0530

    signup

---
+++ public/js/signup.js
@@ -0,0 +1,37 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+// const { createClient } = _supabase;
+
+const supabase = createClient('https://zduoakccvlarenfacitl.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpkdW9ha2NjdmxhcmVuZmFjaXRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDQ5NDU5MTIsImV4cCI6MTk2MDUyMTkxMn0.L0L9_rg1NCok9PJWLS17-xGyWgEyRFj83a0o-D127f4');
+
+$('#btnSignUp').on('click', signUp);
+
+async function signUp() {
+    let email = $('#email').val();
+    let password = $('#password').val();
+    let passwordConfirm = $('#passwordConfirm').val();
+    let username = $('#username').val();
+
+    if (!(email && password && passwordConfirm && username)) {
+        alert('Please fill in all fields');
+    } else {
+
+    if (password===passwordConfirm) {
+        const { user, session, error } = await supabase.auth.signUp({
+            email: email,
+            password: password,
+        },
+        {
+          data: { 
+            username: username, 
+            premium: false
+          }
+        }).then(function() {
+          alert('Sign up successful. Check your email for a verification link.');
+        }).catch(function(error) {
+          alert(error);
+        });
+    }
+
+    }
+
+}
