36d488a15c66ebbb7b1a29ba7029dbb301112e36
Atharva Wasekar <45729391+Electric-Dragon@users.noreply.github.com>
2022-03-10 14:09:59 +0530

    shows day on chapter

--- public/js/readNovel.js
+++ public/js/readNovel.js
@@ -6,6 +6,7 @@ let liked = false;
 let arr = window.location.pathname.split( '/' )
 let chapterid = arr[arr.length - 1];
 let seriesid = arr[arr.length - 2];
+var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 let heartsvg = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
 <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
@@ -44,7 +45,7 @@ $.ajax({
             $('#chapNum').text(chapternum);
 
             let date = new Date(createdat);
-            $('#chapDate').text(`${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`);
+            $('#chapDate').text(`${days[date.getDay()]}, ${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`);
 
             $('#genre1').attr('href',`/genres/${seriesid.genre1}`);
             $('#genre1').text(seriesid.genre1);
