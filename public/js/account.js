781fba6caccdc134cb6acdfb378189d6da96a398
Atharva Wasekar <45729391+Electric-Dragon@users.noreply.github.com>
2022-03-10 13:00:35 +0530

    can connect socials now

--- public/js/account.js
+++ public/js/account.js
@@ -1,12 +1,12 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 import {erroralert, successalert} from '/js/salert.js';
 
-let supabase, publicData, user;
+let supabase, publicData, user, instagramAcc, redditAcc, youtubeAcc;
 
+$('#instaConnected').hide();
+$('#redditConnected').hide();
+$('#youtubeConnected').hide();
 
-if (window.location.href.split('?')[1]) {
-  window.location = "/account"
-}
 
 const swalWithBootstrapButtons = Swal.mixin({
   customClass: {
@@ -15,29 +15,8 @@ const swalWithBootstrapButtons = Swal.mixin({
   },
   buttonsStyling: false
 })
-const { value: url } = await swalWithBootstrapButtons.fire({
-  input: 'url',
-  inputLabel: 'URL address',
-  inputPlaceholder: 'Enter the URL'
-}).then((result) => {
-  if (result.isConfirmed) {
-    swalWithBootstrapButtons.fire(
-      'Connected!',
-      'Your instagram account has been connected.',
-      'success'
-    )
-  } else if (
-    /* Read more about handling dismissals below */
-    result.dismiss === Swal.DismissReason.cancel
-  ) {
-    swalWithBootstrapButtons.fire(
-      'Cancelled',
-      'Your imaginary file is safe :)',
-      'error'
-    )
-  }}
 
-,$.ajax({
+$.ajax({
   url: "/keys",
   success: async function( result ) {
 
@@ -73,6 +52,23 @@ const { value: url } = await swalWithBootstrapButtons.fire({
        let { pfp, username, description, instagram, reddit, youtube } = public_user;
        publicData = public_user;
 
+       instagramAcc = instagram;
+       redditAcc = reddit;
+       youtubeAcc = youtube;
+
+       if (instagram) {
+         $('#instaConnected').show();
+         $('#instaConnected').text(`@${instagram}`);
+       }
+       if (reddit) {
+          $('#redditConnected').show();
+         $('#redditConnected').text(`u/${reddit}`);
+       }
+       if (youtube) {
+          $('#youtubeConnected').show();
+         $('#youtubeConnected').text(`${youtube}`);
+       }
+
        if (private_user) {
 
             let { fName, lName, dob, pNumber, creator } = private_user[0];
@@ -94,7 +90,7 @@ const { value: url } = await swalWithBootstrapButtons.fire({
         $('#pfp').attr('src',pfp);
      }
 
-}}));
+}});
 
 window.saveDetails = async function saveDetails () {
 
@@ -140,6 +136,126 @@ window.saveDetails = async function saveDetails () {
 
 window.connectInstagram = async function connectInstagram () {
 
-  
+  const { value: handle } = await swalWithBootstrapButtons.fire({
+    input: 'text',
+    inputLabel: 'Instagram Handle',
+    inputPlaceholder: '@username',
+    inputValue:instagramAcc
+  }).then(async (result) => {
+    if (result.isConfirmed) {
+
+      result.value = result.value.trim();
+
+      if (result.value === '') {
+        result.value = null        
+      } else if (result.value.startsWith('@')) {
+        result.value = result.value.slice(1);
+      }
+
+      const { data, error } = await supabase
+        .from('public_profile')
+        .update({ instagram: result.value })
+        .match({ id: user.id })
+
+      if (error) {
+        erroralert(error.message);
+      } else {
+
+        let text = result.value ? `Your Instagram account has been connected.` : `Your Instagram account has been disconnected.`;
+
+        swalWithBootstrapButtons.fire(
+          'Success!',
+          text,
+          'success'
+        ).then(() => {
+          window.location.reload();
+        })
+      
+      }
+    }});
+
+}
+
+window.connectReddit = async function connectReddit () {
+
+  const { value: handle } = await swalWithBootstrapButtons.fire({
+    input: 'text',
+    inputLabel: 'Reddit username',
+    inputPlaceholder: 'u/username',
+    inputValue:redditAcc
+  }).then(async (result) => {
+    if (result.isConfirmed) {
+
+      result.value = result.value.trim();
+
+      if (result.value === '') {
+        result.value = null        
+      } else if (result.value.startsWith('u/')) {
+        result.value = result.value.slice(2);
+      }
+
+      const { data, error } = await supabase
+        .from('public_profile')
+        .update({ reddit: result.value })
+        .match({ id: user.id })
+
+      if (error) {
+        erroralert(error.message);
+      } else {
+
+        let text = result.value ? `Your Reddit account has been connected.` : `Your Reddit account has been disconnected.`;
+
+        swalWithBootstrapButtons.fire(
+          'Success!',
+          text,
+          'success'
+        ).then(() => {
+          window.location.reload();
+        })
+      
+      }
+    }});
+
+}
+
+window.connectYoutube = async function connectYoutube () {
+
+  const { value: handle } = await swalWithBootstrapButtons.fire({
+    input: 'text',
+    inputLabel: 'Youtube username',
+    inputPlaceholder: 'c/username',
+    inputValue:youtubeAcc
+  }).then(async (result) => {
+    if (result.isConfirmed) {
+
+      result.value = result.value.trim();
+
+      if (result.value === '') {
+        result.value = null        
+      } else if (result.value.startsWith('c/')) {
+        result.value = result.value.slice(2);
+      }
+
+      const { data, error } = await supabase
+        .from('public_profile')
+        .update({ youtube: result.value })
+        .match({ id: user.id })
+
+      if (error) {
+        erroralert(error.message);
+      } else {
+
+        let text = result.value ? `Your YouTube channel has been connected.` : `Your YouTube channel has been disconnected.`;
+
+        swalWithBootstrapButtons.fire(
+          'Success!',
+          text,
+          'success'
+        ).then(() => {
+          window.location.reload();
+        })
+      
+      }
+    }});
 
 }
