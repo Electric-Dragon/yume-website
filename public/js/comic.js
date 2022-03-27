7f97c59862144a87235bdb197f87555981874e1a
Atharva Wasekar <45729391+Electric-Dragon@users.noreply.github.com>
2022-03-27 19:28:04 +0530

    shows panels preview and size

--- public/js/comic.js
+++ public/js/comic.js
@@ -2,12 +2,11 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 import {erroralert, successalert} from '/js/salert.js';
 
 let supabase, user;
+let size = 0;
 
 let arr = window.location.pathname.split( '/' )
 let chapterid = arr[arr.length - 2];
 let seriesid = arr[arr.length - 3];
-
-console.log(chapterid);
 
 $.ajax({
     url: "/keys",
@@ -40,4 +39,42 @@ $.ajax({
   
         }
   
-  }});
+}});
+
+window.previewPanels = function (e) {
+
+    if (e.target.files.length > 0) {
+
+        let panels = [...e.target.files];
+
+        panels.sort(function(a, b) {
+            a = Number(a.name.split('.')[0]);
+            b = Number(b.name.split('.')[0]);
+            return a - b
+        });
+
+        panels.forEach(file => {
+
+            let link = URL.createObjectURL(file);
+
+            size+=(file.size/1000000);
+
+            let element = `
+                            <div class="w-full p-4 lg:w-50 lg:h-full">
+                                <div class=" bg-white border rounded shadow-sm ">
+                                    <div class="relative">
+                                        <img class="h-60 object-cover " src="${link}" alt="${file.name}">
+                                        <p class="text-gray-600">${file.name}</p>
+                                    </div>                          
+                                </div>
+                            </div>`
+
+            $('#panelPreviewContainer').append(element);
+
+        });
+
+        $('#totalSize').text(`${size.toFixed(2)} MB / 20 MB`);
+
+    }
+
+}
