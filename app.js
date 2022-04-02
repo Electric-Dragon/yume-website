b3be2a8039f6e216f6f9c6fd2237f78cada695ce
MaxTGM <jaimaini3@gmail.com>
2022-04-02 18:57:49 +0530

    popular page

--- app.js
+++ app.js
@@ -107,6 +107,11 @@ app.get("/results", function(req, res) {
     res.sendFile(__dirname + "/html/search.html");
 });
 
+app.get("/popular", function(req, res) {
+    res.sendFile(__dirname + "/html/popular.html");
+});
+
+
 app.get("/read/comic/:seriesid/:chapterid", async function(req, res) {
     await supabase.rpc('incrementchapterreads', { row_id: req.params.chapterid })
     res.sendFile(__dirname + "/html/readComic.html");
