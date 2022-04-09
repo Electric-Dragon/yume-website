3f117943f517b8e48879c115f38bd626a19fc406
Atharva Wasekar <45729391+Electric-Dragon@users.noreply.github.com>
2022-04-09 18:54:03 +0530

    fixed popular page

--- public/js/popular.js
+++ public/js/popular.js
@@ -17,7 +17,7 @@ $.ajax({
 
         const { data, error } = await supabase
             .from('series_popularity')
-            .select('seriesid')
+            .select('series:seriesid(id,title,cover,summary,genre1,genre2,creator(username))')
             .order('popularity_score', { ascending: false })
             .limit(10)
 
@@ -25,83 +25,56 @@ $.ajax({
             erroralert(error.message);
         } else {
 
-            const { data:mostPopular, error: mostPopularError} = await supabase
-            .from('series')
-            .select('title,cover,summary,genre1,genre2,creator(username)')
-            .eq('id', data[0].seriesid)
-            .limit(1)
-            .single();
+          let {id, title, cover, summary, genre1, genre2, creator} = data[0].series;
 
-            if (mostPopularError) {
-                erroralert(mostPopularError.message);
-            } else {
+          $('#mostPopularTitle').text(title);
+          $('#mostPopularTitle').attr('href', `/series/${id}`);
+          $('#mostPopularCover').attr('src', cover);
+          $('#mostPopularSummary').text(summary);
+          $('#mostPopularGenre1').text(genre1);
+          $('#mostPopularGenre2').text(genre2);
+          $('#mostPopularCreator').text(creator.username);
 
-                let {title, cover, summary, genre1, genre2, creator} = mostPopular;
+          data.shift();
 
-                $('#mostPopularTitle').text(title);
-                $('#mostPopularCover').attr('src', cover);
-                $('#mostPopularSummary').text(summary);
-                $('#mostPopularGenre1').text(genre1);
-                $('#mostPopularGenre2').text(genre2);
-                $('#mostPopularCreator').text(creator.username);
+          data.forEach(async (val,index) => {
 
-            }
+              let {id, title, cover, genre1, genre2, creator} = val.series;
 
-            data.shift();
+              let element = `
+                              <tr class="text-gray-700 dark:text-gray-400">
+                                <td class="px-4 py-3">
+                                  <div class="flex items-center text-sm">
+                                    <div
+                                      class="relative  w-8 h-8 mr-3 rounded-full md:block"
+                                    >
+                                      <img
+                                        class="object-cover w-full h-full rounded-sm"
+                                        src="${cover}"
+                                        alt=""
+                                      />
+                                      <div
+                                        class="absolute inset-0 rounded-full shadow-inner"
+                                        aria-hidden="true"
+                                      ></div>
+                                    </div>
+                                    <div>
+                                      <a href="/series/${id}" class="font-semibold"><span class="mr-3 ml-2">${index+2}.</span>${title}</a> 
+                                      <p class="text-xs text-gray-600 dark:text-gray-400 ml-8">
+                                        ${genre1}, ${genre2}
+                                      </p>
+                                    </div>
+                                  </div>
+                                </td>
+                                <td class="px-4 py-3 text-sm">
+                                  ${creator.username}
+                                </td>
+                              </tr>
+              `;
 
-            data.forEach(async (val,index) => {
+              $('#seriesContainer').append(element);
 
-                let {seriesid} = val;
-
-                const { data:series, error: seriesError} = await supabase
-                    .from('series')
-                    .select('title,cover,genre1,genre2,creator(username)')
-                    .eq('id', val.seriesid)
-                    .limit(1)
-                    .single();
-
-                if (seriesError) {
-                    erroralert(seriesError.message);
-                } else {
-
-                    let {title, cover, genre1, genre2, creator} = series;
-
-                    let element = `
-                                    <tr class="text-gray-700 dark:text-gray-400">
-                                      <td class="px-4 py-3">
-                                        <div class="flex items-center text-sm">
-                                          <div
-                                            class="relative  w-8 h-8 mr-3 rounded-full md:block"
-                                          >
-                                            <img
-                                              class="object-cover w-full h-full rounded-sm"
-                                              src="${cover}"
-                                              alt=""
-                                            />
-                                            <div
-                                              class="absolute inset-0 rounded-full shadow-inner"
-                                              aria-hidden="true"
-                                            ></div>
-                                          </div>
-                                          <div>
-                                            <p class="font-semibold"><span class="mr-3 ml-2">${index+2}.</span>${title}</p> 
-                                            <p class="text-xs text-gray-600 dark:text-gray-400 ml-8">
-                                              ${genre1}, ${genre2}
-                                            </p>
-                                          </div>
-                                        </div>
-                                      </td>
-                                      <td class="px-4 py-3 text-sm">
-                                        ${creator.username}
-                                      </td>
-                                    </tr>
-                    `;
-
-                    $('#seriesContainer').append(element);
-
-                }
-
-            });
+          });
 
         }
   
