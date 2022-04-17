97b61166de6b99a02c37619c83681566ae9d84c7
Atharva Wasekar <45729391+Electric-Dragon@users.noreply.github.com>
2022-04-17 20:27:54 +0530

    completed filter popular series by type

--- public/js/popular.js
+++ public/js/popular.js
@@ -3,7 +3,11 @@ import {erroralert, successalert} from '/js/salert.js';
 
 let supabase,user;
 
-let filter = '';
+let popular = {
+  all: null,
+  novels: null,
+  comics: null
+}
 
 $.ajax({
     url: "/keys",
@@ -17,69 +21,143 @@ $.ajax({
 
         const { data, error } = await supabase
             .from('series_popularity')
-            // .select('series:seriesid(id,title,cover,summary,genre1,genre2,creator(username)),series!inner(*)')
             .select('series!inner(id,title,cover,summary,genre1,genre2,creator(username))')
-            .eq('series.novel',false)
+            // .eq('series.novel',false)
             .order('popularity_score', { ascending: false })
             .limit(10)
 
         if (error) {
             erroralert(error.message);
         } else {
-
-          let {id, title, cover, summary, genre1, genre2, creator} = data[0].series;
-
-          $('#mostPopularTitle').text(title);
-          $('#mostPopularTitle').attr('href', `/series/${id}`);
-          $('#mostPopularCover').attr('src', cover);
-          $('#mostPopularSummary').text(summary);
-          $('#mostPopularGenre1').text(genre1);
-          $('#mostPopularGenre2').text(genre2);
-          $('#mostPopularCreator').text(creator.username);
-
-          data.shift();
-
-          data.forEach(async (val,index) => {
-
-              let {id, title, cover, genre1, genre2, creator} = val.series;
-
-              let element = `
-                              <tr class="text-gray-700 dark:text-gray-400">
-                                <td class="px-4 py-3">
-                                  <div class="flex items-center text-sm">
-                                    <div
-                                      class="relative  w-8 h-8 mr-3 rounded-full md:block"
-                                    >
-                                      <img
-                                        class="object-cover w-full h-full rounded-sm"
-                                        src="${cover}"
-                                        alt=""
-                                      />
-                                      <div
-                                        class="absolute inset-0 rounded-full shadow-inner"
-                                        aria-hidden="true"
-                                      ></div>
-                                    </div>
-                                    <div>
-                                      <a href="/series/${id}" class="font-semibold"><span class="mr-3 ml-2">${index+2}.</span>${title}</a> 
-                                      <p class="text-xs text-gray-600 dark:text-gray-400 ml-8">
-                                        ${genre1}, ${genre2}
-                                      </p>
-                                    </div>
-                                  </div>
-                                </td>
-                                <td class="px-4 py-3 text-sm">
-                                  <a href="/user/${creator.username}">
-                                    ${creator.username}
-                                  </a>
-                                </td>
-                              </tr>
-              `;
-
-              $('#seriesContainer').append(element);
-
-          });
-
+            popular.all = data;
+            receivedData(data);
         }
+
+        $('#filterAll').click(async function () {
+
+          if (!popular.all) {
+            const { data, error } = await supabase
+            .from('series_popularity')
+            .select('series!inner(id,title,cover,summary,genre1,genre2,creator(username))')
+            .order('popularity_score', { ascending: false })
+            .limit(10)
+
+            if (error) {
+                erroralert(error.message);
+            } else {
+                popular.all = data;
+                receivedData(data);
+            }
+          } else {
+            receivedData(popular.all);
+          }
+
+        });
+
+        $('#filterNovels').click(async function () {
+
+          if (!popular.novels) {
+            const { data, error } = await supabase
+            .from('series_popularity')
+            .select('series!inner(id,title,cover,summary,genre1,genre2,creator(username))')
+            .eq('series.novel',true)
+            .order('popularity_score', { ascending: false })
+            .limit(10)
+
+            if (error) {
+                erroralert(error.message);
+            } else {
+                popular.novels = data;
+                receivedData(data);
+            }
+          } else {
+            receivedData(popular.novels);
+          }
+
+        });
+
+        $('#filterComics').click(async function () {
+            
+            if (!popular.comics) {
+              const { data, error } = await supabase
+              .from('series_popularity')
+              .select('series!inner(id,title,cover,summary,genre1,genre2,creator(username))')
+              .eq('series.novel',false)
+              .order('popularity_score', { ascending: false })
+              .limit(10)
+  
+              if (error) {
+                  erroralert(error.message);
+              } else {
+                  popular.comics = data;
+                  receivedData(data);
+              }
+            } else {
+              receivedData(popular.comics);
+            }
+  
+        });
   
 }});
+
+async function receivedData(data) {
+  
+  let {id, title, cover, summary, genre1, genre2, creator} = data[0].series;
+
+  $('#mostPopularTitle').text(title);
+  $('#mostPopularTitle').attr('href', `/series/${id}`);
+  $('#mostPopularCover').attr('src', cover);
+  $('#mostPopularSummary').text(summary);
+  $('#mostPopularGenre1').text(genre1);
+  $('#mostPopularGenre2').text(genre2);
+  $('#mostPopularCreator').text(creator.username);
+
+  let first = data.shift();
+
+  $('#seriesContainer').empty();
+
+  data.forEach(showSeries);
+
+  data.unshift(first);
+}
+
+async function showSeries(val, index) {
+
+  let {id, title, cover, genre1, genre2, creator} = val.series;
+
+  let element = `
+                  <tr class="text-gray-700 dark:text-gray-400">
+                    <td class="px-4 py-3">
+                      <div class="flex items-center text-sm">
+                        <div
+                          class="relative  w-8 h-8 mr-3 rounded-full md:block"
+                        >
+                          <img
+                            class="object-cover w-full h-full rounded-sm"
+                            src="${cover}"
+                            alt=""
+                          />
+                          <div
+                            class="absolute inset-0 rounded-full shadow-inner"
+                            aria-hidden="true"
+                          ></div>
+                        </div>
+                        <div>
+                          <a href="/series/${id}" class="font-semibold"><span class="mr-3 ml-2">${index+2}.</span>${title}</a> 
+                          <p class="text-xs text-gray-600 dark:text-gray-400 ml-8">
+                            ${genre1}, ${genre2}
+                          </p>
+                        </div>
+                      </div>
+                    </td>
+                    <td class="px-4 py-3 text-sm">
+                      <a href="/user/${creator.username}">
+                        ${creator.username}
+                      </a>
+                    </td>
+                  </tr>
+  `;
+
+  $('#seriesContainer').append(element);
+
+}
