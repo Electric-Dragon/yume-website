de9b039cba6af36c9b5ea19c7afbe59b2d98531f
Atharva Wasekar <45729391+Electric-Dragon@users.noreply.github.com>
2022-04-24 20:37:35 +0530

    anchor tags for popular creator work

--- public/js/home.js
+++ public/js/home.js
@@ -119,24 +119,26 @@ $.ajax({
               }
               
               let element = `
-              <div class="group relative dark:text-white">
-                    <div class="object-cover aspect-square rounded-full bg-gray-200 aspect-w-1 aspect-h-1 overflow-hidden group-hover:opacity-75">
-                        <img class="aspect-square object-cover h-full" src="${pfp}" class=" object-center object-cover">
-                    </div>
-                    <div class="mt-4 flex justify-between">
-                        <div>
-                            <h3 class="text-sm">
-                            <a href="/series/${id}" class="text-gray-700 font-bold dark:text-gray-100">
-                                <span aria-hidden="true" class="absolute inset-0"></span>
-                                ${username}
-                            </a>
-                            <br>
-                            <span aria-hidden="true" class="text-xs absolute inset-0 text-gray-300"></span>
-                            <p class="text-xs text-gray-500 dark:text-gray-500">${types}</p>
-                            </h3>
-                        </div>
-                    </div>
-              </div>`
+                <div class="group relative dark:text-white">
+                      <div class="object-cover aspect-square rounded-full bg-gray-200 aspect-w-1 aspect-h-1 overflow-hidden group-hover:opacity-75">
+                          <img class="aspect-square object-cover h-full" src="${pfp}" class=" object-center object-cover">
+                      </div>
+                      <div class="mt-4 flex justify-between">
+                          <div>
+                              <h3 class="text-sm">
+                              <a href="/user/${username}" class="text-gray-700 font-bold dark:text-gray-100">
+                                  <span aria-hidden="true" class="absolute inset-0"></span>
+                                  ${username}
+                              </a>
+                              <br>
+                              <a href="/user/${username}">
+                                <span aria-hidden="true" class="text-xs absolute inset-0 text-gray-300"></span>
+                              </a>
+                              <p class="text-xs text-gray-500 dark:text-gray-500">${types}</p>
+                              </h3>
+                          </div>
+                      </div>
+                </div>`
 
               $("#popularCreatorsDiv").append(element);
 
