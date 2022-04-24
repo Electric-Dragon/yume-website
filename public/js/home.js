d42f57188ff608184145df1959d50d4666a2b6b0
Atharva Wasekar <45729391+Electric-Dragon@users.noreply.github.com>
2022-04-24 20:29:25 +0530

    shows top creators

--- public/js/home.js
+++ public/js/home.js
@@ -13,7 +13,7 @@ const Toast = Swal.mixin({
     toast.addEventListener('mouseenter', Swal.stopTimer)
     toast.addEventListener('mouseleave', Swal.resumeTimer)
   }
-})
+});
 
 if (saveInfo) {
 
@@ -30,6 +30,11 @@ if (saveInfo) {
 }
 
 let supabase,user;
+
+let creatorType = {
+  'a': 'Artist',
+  'w': 'Writer'
+}
 
 $.ajax({
     url: "/keys",
@@ -79,8 +84,66 @@ $.ajax({
   
             $('#recentlyPub').append(element);
   
-          })
+          });
   
+        }
+
+        const { data:creators, error:creatorsError } = await supabase
+            .from('series_popularity')
+            .select('series!inner(creator(id,username,pfp,creator_type))')
+            .order('popularity_score', { ascending: false })
+            .limit(5)
+
+        if (creatorsError) {
+            erroralert(creatorsError.message);
+        } else {
+
+          let uniqueCreators = [];
+
+          creators.forEach((creator)=> {
+
+            let {series:{creator:{id,username,pfp,creator_type}}} = creator;
+
+            if (!uniqueCreators.includes(username)) {
+              uniqueCreators.push(username);
+
+              let types = '';
+
+              if (creator_type) {
+                creator_type.forEach((type,index)=> {
+                  types+=creatorType[type];
+                  if (index === 0 && creator_type.length > 1) {
+                    types+=' & ';
+                  }
+                })
+              }
+              
+              let element = `
+              <div class="group relative dark:text-white">
+                    <div class="object-cover aspect-square rounded-full bg-gray-200 aspect-w-1 aspect-h-1 overflow-hidden group-hover:opacity-75">
+                        <img class="aspect-square object-cover h-full" src="${pfp}" class=" object-center object-cover">
+                    </div>
+                    <div class="mt-4 flex justify-between">
+                        <div>
+                            <h3 class="text-sm">
+                            <a href="/series/${id}" class="text-gray-700 font-bold dark:text-gray-100">
+                                <span aria-hidden="true" class="absolute inset-0"></span>
+                                ${username}
+                            </a>
+                            <br>
+                            <span aria-hidden="true" class="text-xs absolute inset-0 text-gray-300"></span>
+                            <p class="text-xs text-gray-500 dark:text-gray-500">${types}</p>
+                            </h3>
+                        </div>
+                    </div>
+              </div>`
+
+              $("#popularCreatorsDiv").append(element);
+
+            }
+
+          });
+
         }
   
   }});
