463a1debf783ad2ab996b705a2fe2447b873eda5
MaxTGM <jaimaini3@gmail.com>
2022-04-25 17:07:53 +0530

    search

--- public/js/search.js
+++ public/js/search.js
@@ -121,14 +121,30 @@ function showElement(series) {
 
   let typeText = novel ? 'Web Novel' : 'Web Comic';
 
-  let element = `<a href="/series/${id}">
+  let element = `
   <div class="max-w-lg bg-white border-2 border-gray-300 p-5 rounded-md tracking-wide shadow-lg">
     <div id="header" class="grid grid-flow-col"> 
-        <div class=" aspect-square object-cover bg-gray-200 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75">
-            <img class="object-cover" src="${cover}" >
-          </div>                       
+    <div class="group relative dark:text-white">
+    <div class="object-cover aspect-square bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75">
+        <img class="aspect-square object-cover h-full" src="${cover}" class=" object-center object-cover">
+    </div>
+    <div class="mt-4 flex justify-between">
+        <div>
+            <h3 class="text-sm">
+            <a href="/series/" class="text-gray-700 font-bold dark:text-gray-100">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+            </a>
+            <br>
+            <a href="/series/">
+            <span aria-hidden="true" class="text-xs absolute inset-0 text-gray-300"></span>
+            <p class="text-xs text-gray-500 dark:text-gray-500">by</p>
+            </a>
+            </h3>
+        </div>
+    </div>
+</div>                    
           <div id="body" class="flex flex-col ml-5">
-          <h4 id="name" class="text-xl font-semibold mb-2">${title}</h4>
+          <a href="/series/${id}"> <h4 id="name" class="text-xl font-semibold mb-2">${title}</h4></a>
           <p id="description" class="text-gray-800 mt-2">${summary}</p>
           <div class="flex mt-5">
             <p>Author:</p>
@@ -145,8 +161,7 @@ function showElement(series) {
         </div>
       </div>
     </div>
-  </div>
-  </a>`
+  </div>`
 
   $('#searchResults').append(element);
 
