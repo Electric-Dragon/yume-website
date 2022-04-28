cb8204850434b8daf9abd6ce8f1d6ef18180be88
Atharva Wasekar <45729391+Electric-Dragon@users.noreply.github.com>
2022-04-28 18:54:45 +0530

    can upload sample arts

--- public/js/only.js
+++ public/js/only.js
@@ -2,6 +2,8 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 import {erroralert, successalert} from '/js/salert.js';
 
 let supabase, publicData, user, instagramAcc, redditAcc, youtubeAcc;
+let size = 0;
+let panels, publicURLS = [];
 
 $('#instaConnected').hide();
 $('#redditConnected').hide();
@@ -70,7 +72,7 @@ $.ajax({
 
        $('#toggle').prop('checked',is_creator);
 
-       if (creator_type && creator_type === ["a"]) {
+       if (creator_type && creator_type[0] === "a") {
           $('#artistSamplesSection').show();
        }
 
@@ -87,11 +89,26 @@ $.ajax({
 
         $('#description').val(description);
 
+        sample_arts.forEach(image => {
+          let element = `
+                      <div class="w-full p-4 lg:w-50 lg:h-full">
+                          <div class=" bg-white border rounded shadow-sm ">
+                              <div class="relative">
+                                  <img class="h-60 object-cover " src="${image}">
+                              </div>                          
+                          </div>
+                      </div>`
+
+          $('#panelPreviewContainer').append(element);
+      })
+
      }
 
 }});
 
-window.saveDetails = async function saveDetails () {
+window.saveDetails = async function saveDetails (e) {
+
+  e.preventDefault();
 
   let descriptionNew = $('#description').val();
 
@@ -116,10 +133,64 @@ window.saveDetails = async function saveDetails () {
 
   }
 
+  if (size > 10) {
+    erroralert('Total size of uploaded files is greater than 10 MB');
+    return;
+  }
+  if (panels.length === 0) {
+      erroralert('Please upload at least one sample');
+      return;
+  }
+
+  $('#btnSaveDetails').prop('disabled', true);
+
+  for (const file of panels) {
+
+      let route = `${user.id}/profile/samples/${file.name}`;
+
+      const { data, error } = await supabase
+      .storage
+      .from('users')
+      .upload(route, file, {
+          cacheControl: '3600',
+          upsert: true
+      });
+
+      if (error) {
+          erroralert(error.message);
+          $('#btnSaveDetails').prop('disabled', false);
+      } else {
+          const { publicURL, error:error_ } = supabase
+              .storage
+              .from('users')
+              .getPublicUrl(route);
+          
+          if (error_) {
+              erroralert(error_.message);
+              $('#btnSaveDetails').prop('disabled', false);
+          } else {
+              publicURLS.push(publicURL);
+          }
+          
+      }   
+  }
+
+  const {data:data_, error:error_} = await supabase.from('public_profile')
+          .update({ sample_arts: publicURLS })
+          .match({ id: user.id });
+
+  if (error_) {
+      erroralert(error.message);
+      $('#btnSaveDetails').prop('disabled', false);
+  } else {
+      successalert(`Changes made successfully`, function() {
+          window.location = "/dashboard/creator"
+      });
+  }
+
 }
 
 window.updateCreatorType = async function updateCreatorType(e) {
-  console.log(e.target.value);
 
   const { data, error } = await supabase
     .from('public_profile')
@@ -263,4 +334,63 @@ window.connectYoutube = async function connectYoutube () {
       }
     }});
 
+}
+
+window.previewPanels = function (e) {
+
+  if (e.target.files.length > 0) {
+
+      panels = [...e.target.files];
+
+      panels.sort(function(a, b) {
+          a = Number(a.name.split('.')[0]);
+          b = Number(b.name.split('.')[0]);
+          return a - b
+      });
+
+      panels.forEach((file,index) => {
+
+          let link = URL.createObjectURL(file);
+
+          size+=(file.size/1000000);
+
+          let element = `
+                          <div class="w-full p-4 lg:w-50 lg:h-full">
+                              <div class=" bg-white border rounded shadow-sm ">
+                                  <div class="relative">
+                                      <div style="z-index:4" onclick="deleteOne(${file.name.split('.')[0]})" class=" top-0 right-0 p-0 m-0 absolute">
+                                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"  fill="currentColor" class="text-red-500  " viewBox="0 0 0.1 9">
+                                              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
+                                          </svg>
+                                      </div>
+                                  </div>
+                                  <div class="relative">
+                                      <img class="h-60 object-cover " src="${link}" alt="${file.name}">
+                                      <p class="text-gray-600">${file.name}</p>
+                                  </div>                          
+                              </div>
+                          </div>`
+
+          $('#panelPreviewContainer').append(element);
+
+      });
+
+      $('#totalSize').text(`${size.toFixed(2)} MB / 10 MB`);
+
+  }
+
+}
+
+window.deleteAll = function() {
+  $('#panelPreviewContainer').empty();
+  $('#totalSize').text('0 MB / 20 MB');
+  size = 0;
+  panels = [];
+  $('#panels').prop('files', []);
+}
+
+window.deleteOne = function(num) {
+  let index = panels.findIndex(panel => panel.name === `${num}.jpg`);
+  panels.splice(index,1);
+  $('#panelPreviewContainer').children().eq(index).remove();
 }
