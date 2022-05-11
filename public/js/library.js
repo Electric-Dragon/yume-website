62ba8b5c7a106676fb793037d74653ac03e4790c
Atharva Wasekar <45729391+Electric-Dragon@users.noreply.github.com>
2022-05-11 22:40:36 +0530

    library looks like home page you wouldn't even realise

--- public/js/library.js
+++ public/js/library.js
@@ -19,7 +19,7 @@ $.ajax({
   
         const { data, error } = await supabase
           .from('series_follows')
-          .select('series(id,title,cover)')
+          .select('series(id,title,cover,creator(username))')
           .eq('user', user.id)
         //   .limit(8)
   
@@ -29,18 +29,23 @@ $.ajax({
   
           data.forEach(val=> {
   
-            let {id, title, cover} = val.series;
+            let {series:{id, title, cover, creator:{username}}} = val;
   
-            let element = `<div class="group relative">
+            let element = `<div class="group relative dark:text-white">
                                 <div class="object-cover aspect-square bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75">
                                     <img class="aspect-square object-cover h-full" src="${cover}" class=" object-center object-cover">
                                 </div>
                                 <div class="mt-4 flex justify-between">
                                     <div>
-                                        <h3 class="text-sm text-gray-700 font-bold">
-                                        <a href="/series/${id}">
+                                        <h3 class="text-sm">
+                                        <a href="/series/${id}" class="text-gray-700 font-bold dark:text-gray-100">
                                             <span aria-hidden="true" class="absolute inset-0"></span>
                                             ${title}
+                                        </a>
+                                        <br>
+                                        <a href="/series/${id}">
+                                        <span aria-hidden="true" class="text-xs absolute inset-0 text-gray-300"></span>
+                                        <p class="text-xs text-gray-500 dark:text-gray-500">by ${username}</p>
                                         </a>
                                         </h3>
                                     </div>
