9fd0a5cf941dbc1d5910fb3a300da231929fbe20
Atharva Wasekar <45729391+Electric-Dragon@users.noreply.github.com>
2022-05-13 16:25:09 +0530

    completed genre page

--- public/js/genre.js
+++ public/js/genre.js
@@ -17,7 +17,7 @@ $.ajax({
   
         const { data, error } = await supabase
           .from('series')
-          .select('id,title,cover')
+          .select('id,title,cover,creator(username)')
           .neq('status', 'd')
           .order('updatedat', { ascending: false })
           .or(`genre1.eq.${genre},genre2.eq.${genre}`)
@@ -29,7 +29,7 @@ $.ajax({
   
           data.forEach(val=> {
   
-            let {id, title, cover} = val;
+            let {id, title, cover, creator:{username}} = val;
   
             let element = `<div class="group relative dark:text-white">
                                 <div class="object-cover aspect-square bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75">
@@ -37,10 +37,15 @@ $.ajax({
                                 </div>
                                 <div class="mt-4 flex justify-between">
                                     <div>
-                                        <h3 class="text-sm text-gray-700 font-bold dark:text-gray-100">
-                                        <a href="/series/${id}">
+                                        <h3 class="text-sm">
+                                        <a href="/series/${id}" class="text-gray-700 font-bold dark:text-gray-100">
                                             <span aria-hidden="true" class="absolute inset-0"></span>
                                             ${title}
+                                        </a>
+                                        <br>
+                                        <a href="/series/${id}">
+                                        <span aria-hidden="true" class="text-xs absolute inset-0 text-gray-300"></span>
+                                        <p class="text-xs text-gray-500 dark:text-gray-500">by ${username}</p>
                                         </a>
                                         </h3>
                                     </div>
@@ -49,8 +54,51 @@ $.ajax({
   
             $('#recentlyPub').append(element);
   
-          })
+          });
   
+        }
+
+        const {data:popularSeries, error:popularSeriesError} = await supabase
+                .from('series_popularity')
+                .select('series!inner(cover,title,id,creator(username))')
+                .order('popularity_score', { ascending: false })
+                .or(`genre1.eq.${genre},genre2.eq.${genre}`, { foreignTable: "series" })
+                .neq('series.status', 'd')
+                .limit(6)
+        if (popularSeriesError) {
+          erroralert(popularSeriesError.message);
+          console.log(popularSeriesError);
+        } else {
+
+          popularSeries.forEach(val=> {
+
+            let {series: {id, title, cover, creator:{username}}} = val;
+
+            let element = `<div class="group relative dark:text-white">
+                    <div class="object-cover aspect-square bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75">
+                        <img class="aspect-square object-cover h-full" src="${cover}" class=" object-center object-cover">
+                    </div>
+                    <div class="mt-4 flex justify-between">
+                        <div>
+                            <h3 class="text-sm">
+                            <a href="/series/${id}" class="text-gray-700 font-bold dark:text-gray-100">
+                                <span aria-hidden="true" class="absolute inset-0"></span>
+                                ${title}
+                            </a>
+                            <br>
+                            <a href="/series/${id}">
+                            <span aria-hidden="true" class="text-xs absolute inset-0 text-gray-300"></span>
+                            <p class="text-xs text-gray-500 dark:text-gray-500">by ${username}</p>
+                            </a>
+                            </h3>
+                        </div>
+                    </div>
+                </div>`
+  
+            $('#popularSeries').append(element);
+
+          });
+
         }
   
   }});
